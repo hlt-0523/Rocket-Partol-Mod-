98c2b0c48ae7270221df9a6a4717d810512cab14
LingtianHe <lhe80@ucsc.edu>
2023-10-25 21:38:35 -0700

    flip

--- src/prefabs/Spaceship.js
+++ src/prefabs/Spaceship.js
@@ -6,10 +6,13 @@ class Spaceship extends Phaser.GameObjects.Sprite {
         this.points = pointValue;
         this.moveSpeed = game.settings.spaceshipSpeed;
         this.direction = Math.random() < 0.5 ? 'left' : 'right';
+
+        // Set the flip based on initial direction
+        this.updateFlip();
     }
 
     update() {
-
+        // Set the flip based on direction
         this.setFlipX(this.direction === 'right');
         
         if (this.direction === 'left') {
@@ -17,17 +20,30 @@ class Spaceship extends Phaser.GameObjects.Sprite {
 
             if (this.x < -this.width) {
                 this.x = game.config.width;
+                this.direction = Math.random() < 0.5 ? 'left' : 'right';
             }
         } else {
             this.x += this.moveSpeed;
 
             if (this.x > game.config.width) {
                 this.x = -this.width;
+                this.direction = Math.random() < 0.5 ? 'left' : 'right';
             }
         }
     }
 
     reset() {
         this.x = game.config.width;
+        this.direction = Math.random() < 0.5 ? 'left' : 'right';
+        this.updateFlip();
     }
+
+    updateFlip() {
+        if (this.direction === 'right') {
+            this.scaleX = -1;  // flip the sprite to the right
+        } else {
+            this.scaleX = 1;   // original scale for left direction
+        }
+    }
+    
 }
