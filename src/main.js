465ee30cc9e5cd3cb2e356c539d36bd945a3c7bb
LingtianHe <lhe80@ucsc.edu>
2023-10-25 22:05:37 -0700

    updated

--- src/main.js
+++ src/main.js
@@ -1,3 +1,32 @@
+//I am Lingtian HE
+//mod title UFO with Upsets(UwU)
+//I think I spent at least 8 hour for this project (3 hour for the flip BUG)
+/*
+1. Create a new enemy Spaceship type that's smaller, moves faster, and is worth more points:
+
+2. Implement a new timing/scoring mechanism that adds time to the clock for successful hits:
+ 
+3. Randomize each spaceship's movement direction at the start of each play:
+  
+4. Create 4 new explosion sound effects and randomize which one plays on impact:
+ 
+5. Create a new title screen:
+ 
+6. Display the time remaining (in seconds) on the screen:
+
+
+in corresponding:
+Create a new enemy Spaceship type (w/ new artwork) that's smaller, moves faster, and is worth more points (5)
+Implement a new timing/scoring mechanism that adds time to the clock for successful hits (5)
+Randomize each spaceship's movement direction at the start of each play (1)
+Create 4 new explosion sound effects and randomize which one plays on impact (3)
+Create a new title screen (e.g., new artwork, typography, layout) (3)
+Display the time remaining (in seconds) on the screen (3)
+
+*/
+
+
+
 let config = {
   type: Phaser.CANVAS,
   width: 640,
